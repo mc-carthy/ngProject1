e4df12086de7671593a52580456010f6320cd856
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-17 22:53:57 +0100

    Exploring property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,18 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular</h1><courses></courses><authors></authors>',
-    directives: [CoursesComponent, AuthorsComponent]
+    template: `
+        <h1>{{ title }}</h1>
+        
+        <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl" />
+        <img bind-src="imageUrl" />
+    `
 })
-export class AppComponent { }
+
+export class AppComponent
+{ 
+    title = "Angular App";
+    imageUrl = "http://lorempixel.com/400/200/";
+}
