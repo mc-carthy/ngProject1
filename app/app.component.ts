d53c22f61b8bfd1d770f5efbfaf2131f96d95f4a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-19 22:30:43 +0100

    Using ng-content

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,18 @@
 import {Component} from 'angular2/core';
+import {BootstrapPanel} from './bootstrap.panel.component';
 
 @Component({
     selector: 'my-app',
     template: `
-        <ul>
-            <li>Title: {{ task.title }}</li>
-            <li>Assigned to: {{ task.assignee?.name }}</li>
-        </ul>
-    `
+        <bs-panel>
+            <div class="heading">This is the heading!</div>
+            <div class="body">This is the body!</div>
+            <div class="body">This is another body!</div>
+        </bs-panel>
+    `,
+    directives: [BootstrapPanel]
 })
 
 export class AppComponent
 {
-    task = {
-        title: "Review Applications",
-        assignee: {
-            name: "Mick"
-        }
-    };
 }
