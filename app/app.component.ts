4e5749675888ad6a4ee1d38ee0ef6d9a141d6d18
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-21 13:00:42 +0100

    Implement basic template form

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,12 @@
 import {Component} from 'angular2/core';
-import {ZippyComponent} from './zippy.component';
+import {ContactFormComponent} from './contact-form.component'
 
 @Component({
     selector: 'my-app',
     template: `
-        <zippy title="Who can see my stuff?">
-            Content of who can see my stuff
-        </zippy>
-        <zippy title="Who can contact me?">
-            Content of who can contact me
-        </zippy>
+        <contact-form></contact-form>
     `,
-    directives: [ZippyComponent]
+    directives: [ContactFormComponent]
 })
 
 export class AppComponent
