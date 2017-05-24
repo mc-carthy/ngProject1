d7f93904d1f26b09d4bb931c801b93286dd4ca3a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-24 23:43:54 +0100

    Route configuration

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './albums.component', './contact.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1, albums_component_1, contact_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (albums_component_1_1) {
+                albums_component_1 = albums_component_1_1;
+            },
+            function (contact_component_1_1) {
+                contact_component_1 = contact_component_1_1;
             }],
         execute: function() {
             let AppComponent = class AppComponent {
             };
             AppComponent = __decorate([
+                router_1.RouteConfig([
+                    { path: '/albums', name: 'Albums', component: albums_component_1.AlbumsComponent, useAsDefault: true },
+                    { path: '/contact', name: 'Contact', component: contact_component_1.ContactComponent },
+                    { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
+                ]),
                 core_1.Component({
                     selector: 'my-app',
                     templateUrl: '/app/app.component.html'
