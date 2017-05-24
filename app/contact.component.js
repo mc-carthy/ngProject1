79c18a151b79004a843fdeae7687030b3b68411b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-25 00:01:43 +0100

    Implement imperitive navigation

--- app/contact.component.js
+++ app/contact.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,24 +10,31 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1;
     var ContactComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             let ContactComponent = class ContactComponent {
+                constructor(_router) {
+                    this._router = _router;
+                }
                 onSubmit(form) {
                     console.log(form);
+                    this._router.navigate(['Albums']);
                 }
             };
             ContactComponent = __decorate([
                 core_1.Component({
                     templateUrl: '/app/contact.component.html'
                 }), 
-                __metadata('design:paramtypes', [])
+                __metadata('design:paramtypes', [router_1.Router])
             ], ContactComponent);
             exports_1("ContactComponent", ContactComponent);
         }
